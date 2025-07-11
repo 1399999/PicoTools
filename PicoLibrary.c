09d3b9fa55d26414568aba9b7fbc6ad31710a331
1399999 <mzhebrunov@gmail.com>
2025-07-11 10:19:35 -0400

    Adds ADC GPIO functionality.

--- PicoLibrary.c
+++ PicoLibrary.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "PicoLibrary.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 // Pico W devices use a GPIO on the WIFI chip for the LED,
 // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined.
@@ -13,16 +15,23 @@ int main()
 {
     while(true)
     {
+        write_line("Example 1\n");
+
         write_line("Hello, world!");
 
+        write_line("\nExample 2\n");
+
         set_led(true);
-        sleep(1000);
+        sleep(500);
 
         set_led(false);
-        sleep(1000);
+        sleep(500);
+
+        write_line("\nExample 3\n");
     }
 }
 
+#pragma region Basic Functions
 void write(string str)
 {
     if (!is_stdio_init)
@@ -75,4 +84,47 @@ void set_led(bool led_on)
         // Ask the wifi "driver" to set the GPIO on or off
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
     #endif
+}
+
+#pragma endregion
+
+uint16_t read_gpio_pin_adc_raw(uint8_t adc_input)
+{
+    if (!is_adc_init)
+    {
+        adc_init();
+        is_adc_init = true;
+    }
+
+    if (!contains_uint8_t(temp_used_adc_gpio_pins, adc_input + 26))
+    {
+        // Make sure GPIO is high-impedance, no pullups etc.
+        adc_gpio_init(adc_input + 26);
+
+        temp_used_adc_gpio_pins[temp_adc_gpio_index++] = adc_input + 26;
+    }
+
+    // Select ADC input 0 (GPIO26), input 1 (GPIO27) ....
+    adc_select_input(adc_input);
+
+    return adc_read();
+}
+
+float read_gpio_pin_adc_volts(uint8_t adc_input)
+{
+    const float conversion_factor = 3.3f / (1 << 12);
+    return read_gpio_pin_adc_raw(adc_input) * conversion_factor;
+}
+
+bool contains_uint8_t(uint8_t array[], uint8_t value)
+{
+    for (uint8_t i = 0; i < temp_adc_gpio_index; i++)
+    {
+        if (value == array[i])
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
--- PicoLibrary.h
+++ PicoLibrary.h
@@ -9,10 +9,24 @@ bool is_led_init = false;
     bool is_pico_w = false;
 #endif
 
+bool is_adc_init = false;
+
+uint8_t temp_used_adc_gpio_pins[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t * used_adc_gpio_pins = temp_used_adc_gpio_pins;
+uint8_t temp_adc_gpio_index = 0;
+
 int main();
 
+// Basic functions
 void write(string str);
 void write_line(string str);
 void sleep(uint32_t milliseconds);
 int led_init();
 void set_led(bool led_on);
+
+// ADC functions
+uint16_t read_gpio_pin_adc_raw(uint8_t adc_input);
+float read_gpio_pin_adc_volts(uint8_t adc_input);
+
+// Utility functions
+bool contains_uint8_t(uint8_t array[], uint8_t value);
